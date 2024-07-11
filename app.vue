9c016327c8e1ed4b99e03e00e8f8c362bd3e1137
GerstuPA <philipp-alexander.gerstung@basf.com>
2024-07-11 14:32:00 +0200

    Added first pages

--- app.vue
+++ app.vue
@@ -1,6 +1,35 @@
+<script setup lang="ts">
+const router = useRouter();
+const navigate = (to: string) => {
+  router.push(to);
+};
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="main-container">
+    <div class="top-bar">
+      <el-button-group>
+        <el-button round @click="navigateTo('/')">Home</el-button>
+        <el-button round @click="navigateTo('/stations')"
+          >Tankstellen</el-button
+        >
+        <el-button round @click="navigateTo('/about')">Über</el-button>
+      </el-button-group>
+    </div>
+    <NuxtPage />
   </div>
 </template>
+
+<style lang="scss" scoped>
+.main-container {
+  width: 100%;
+  height: 100%;
+}
+
+.tob-bar {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+</style>
---
+++ pages/stations.vue
@@ -0,0 +1,1 @@
+<template>Tankstellen</template>
