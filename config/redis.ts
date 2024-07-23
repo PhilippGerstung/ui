b617b401cc23e64414d1ca1b9f8c0edee35108b2
Philipp Gerstung <mail@philippgerstung.de>
2024-07-23 22:08:57 +0200

    Load plz data into redis cache

--- config/redis.ts
+++ config/redis.ts
@@ -1,9 +1,29 @@
 import { createClient } from 'redis';
+import path from "path";
+import fs from "fs";
+import csv from 'csv-parser';
 
 const client = createClient({
   password: 'eYVX7EwVmmxKPCDmwMtyKVge8oLd2t81'
 });
 client.on('error', (err) => console.log('Redis Client Error', err));
-client.connect().then(() => console.log('Redis Connected'));
+client.connect().then(() => {
+  console.log('Redis Connected');
+    // Path to the CSV file
+const csvFilePath = path.join('./data/plz.csv');
+
+// Read and process the CSV file
+  fs.createReadStream(csvFilePath)
+    .pipe(csv({ separator: ';' }))
+    .on('data', (row) => {
+      const key = `plz_${row["plz"]}`; // Use the first cell as the key with prefix 'plz_'
+      client.set(key, JSON.stringify(row));
+    })
+    .on('end', () => {
+      console.log('CSV file successfully processed');
+      client.quit();
+    });
+  }
+);
 
 export default client;
--- server/api/currentPricesAtPostCode.get.ts
+++ server/api/currentPricesAtPostCode.get.ts
@@ -14,17 +14,14 @@ export default defineEventHandler(async (event): Promise<ExtendedStationPrices> 
 
   const cached = await redis.get(`prices:${lat.toFixed(2)}:${long.toFixed(2)}`);
   if(cached){
-    console.log("Cache hit", cached);
     const res: ExtendedStationPrices = JSON.parse(cached);
     res.cached = true;
     return res;
   }
 
   else {
-    console.log("Cache miss");
-    const prices = await getPricesForLocation(lat, long);
+    const prices = await getPricesForLocation(lat, long, 10);
     await redis.set(`prices:${lat.toFixed(2)}:${long.toFixed(2)}`, JSON.stringify(prices), { EX: REDIS_CACHE_PRICES_EXPIRATION_SECS });
-    console.log("Cached prices", prices);
     const res: ExtendedStationPrices = {...prices, cached: false};
     return res;
   }
