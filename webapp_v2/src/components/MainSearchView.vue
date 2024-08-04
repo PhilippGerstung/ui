cb16b8db7719480fddf6a5dee2aa375d235fa159
Philipp Gerstung <mail@philippgerstung.de>
2024-08-04 08:43:23 +0200

    Added v2 frontend

---
+++ webapp_v2/src/components/MainSearchView.vue
@@ -0,0 +1,21 @@
+<template>
+
+    <section class="hero">
+        <div class="hero-body">
+            <p class="title">Preisabfrage</p>
+            <p class="subtitle">
+                <input class="input" type="text" placeholder="PLZ oder Stadt" v-model="userSearch" />
+            </p>
+        </div>
+
+        {{ userSearch }}
+    </section>
+
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+
+const userSearch = ref("");
+</script>
---
+++ webapp_v2/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import MainSearchView from '../components/MainSearchView.vue'
+</script>
+
+<template>
+  <main>
+    <MainSearchView />
+  </main>
+</template>
