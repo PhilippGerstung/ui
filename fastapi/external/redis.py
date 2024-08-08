9db320cb5287b8c0d01c30652318622fa24f7b4c
Philipp Gerstung <mail@philippgerstung.de>
2024-08-08 18:10:51 +0200

    Fix redis issues

--- fastapi/external/redis.py
+++ fastapi/external/redis.py
@@ -1,7 +1,7 @@
 import redis
 from loguru import logger
 
-from external.environ import USE_FAKE_REDIS
+from external.environ import USE_FAKE_REDIS, REDIS_URL, REDIS_PORT, REDIS_PASSWORD
 
 if USE_FAKE_REDIS:
     import fakeredis
@@ -9,4 +9,4 @@ if USE_FAKE_REDIS:
     redis_db = fakeredis.FakeRedis()
 else:
     logger.info("Using real redis")
-    redis_db = redis.Redis(host="localhost", port=6379, db=0)
+    redis_db = redis.Redis(host=REDIS_URL, port=REDIS_PORT, db=0, password=REDIS_PASSWORD)
