66dae69197b1a5e4dc37aad4b96c0195343534f1
GerstuPA <philipp-alexander.gerstung@basf.com>
2024-08-09 13:51:32 +0200

    Made some small adaptions

--- webapp_v2/src/api/currentPrices.ts
+++ webapp_v2/src/api/currentPrices.ts
@@ -1,8 +1,13 @@
-import { LocationPricesSchema, type LocationPrices } from "@/types/currentPrices";
+import { BACKEND_URL } from '@/config/envVars'
+import { LocationPricesSchema, type LocationPrices } from '@/types/currentPrices'
 
-
-export async function getCurrentPricesForLocation(latitude: number, longitude: number): Promise<LocationPrices> {
-    const response = await fetch(`http://localhost:8000/prices/current_prices?lat=${latitude}&lon=${longitude}`);
-    const data = await response.json();
-    return LocationPricesSchema.parse(data);
+export async function getCurrentPricesForLocation(
+  latitude: number,
+  longitude: number
+): Promise<LocationPrices> {
+  const response = await fetch(
+    `${BACKEND_URL}/prices/current_prices?lat=${latitude}&lon=${longitude}`
+  )
+  const data = await response.json()
+  return LocationPricesSchema.parse(data)
 }
