028502bc7a00f8e571e755ab9d540352ff5cbf37
Philipp Gerstung <mail@philippgerstung.de>
2024-08-11 17:05:56 +0200

    Fix typo in URL

--- webapp_v2/src/config/envVars.ts
+++ webapp_v2/src/config/envVars.ts
@@ -1,1 +1,1 @@
-export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL ?? "https://gpv.dsnhome.de/gpv/api";
+export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL ?? "https://gpv.dnshome.de/gpv/api";
