fb402139fa4238a2e2f3fee3654ef8bee4d3e0e5
Philipp Gerstung <mail@philippgerstung.de>
2024-08-20 16:03:48 +0200

    Add sorting by distance and price

--- webapp_v2/src/components/MainSearchView.vue
+++ webapp_v2/src/components/MainSearchView.vue
@@ -3,7 +3,7 @@
     <section class="hero">
         <div class="hero-body">
             <p class="title">Preisabfrage</p>
-            <div class="is-flex is-flex-direction-row">
+            <div class="is-flex is-flex-direction-row mb-2">
                 <button class="button" @click="tryGetGeoLocation">
                     <span class=" icon is-small">
                         <i class="fas fa-map-marker-alt"></i>
@@ -16,10 +16,23 @@
                     </span>
                 </button>
             </div>
+            <div class="is-flex is-flex-direction-row is-justify-content-flex-end">
+
+                <button class="button mr-2" :class="sortBy.includes(filter.id) ? 'is-primary' : ''"
+                    @click="addRemoveFilter(filter.id)" v-for="filter in filters" :key="filter.id">
+                    <span class=" icon is-small">
+                        <i class="fas fa-sort-amount-down"></i>
+                    </span>
+                    <span>
+                        {{ filter.name }}
+                        <span v-show="sortBy.includes(filter.id)">({{ sortBy.indexOf(filter.id) + 1 }})</span>
+                    </span>
+                </button>
+            </div>
         </div>
-        <div v-if="currentPrices">
+        <div v-if="currentPrices" class="hero-body">
             <div class="card" v-for="item in currentPricesSorted" :key="item.id">
-                <div class="card-content">
+                <div class="card-content p-0">
                     <div class="media">
                         <div class="media-left">
                             <figure class="image is-48x48">
@@ -62,21 +75,63 @@ import { computed, onMounted, ref } from 'vue';
 import { kmDistanceBetweenCoordinates } from "@/helper/geoHelper"
 import { selectedGasType } from '@/stores/userSettings';
 
+
+type PossibleFilters = "price" | "distance";
+type Filters = Array<{ id: PossibleFilters, name: string }>
+
+const filters: Filters = [
+    {
+        id: "price",
+        name: "Preis"
+    },
+    {
+        id: "distance",
+        name: "Entfernung"
+    }
+]
+
 const userSearch = ref("");
 const geoLocationAvailable = ref(true);
 const geoLocation = ref<{ latitude: number; longitude: number } | null>(null);
 const currentPrices = ref<LocationPrices | null>(null);
+const sortBy = ref<Array<PossibleFilters>>(["price"]);
 
 const currentPricesSorted = computed<Station[] | null>(() => {
     if (currentPrices.value == null) {
         return null;
     }
 
-    const sorted = [...currentPrices.value.stations].sort((a, b) => {
+    let sorted = [...currentPrices.value.stations].sort((a, b) => {
         return a[selectedGasType.value] - b[selectedGasType.value];
     });
+
+    for (const filter of [...sortBy.value].reverse()) {
+        if (filter === "price") {
+            sorted = sorted.sort((a, b) => {
+                return a[selectedGasType.value] - b[selectedGasType.value];
+            });
+        }
+        else if (filter === "distance") {
+            sorted = sorted.sort((a, b) => {
+                if (geoLocation.value == null) {
+                    return 0;
+                }
+                return distance(a.lat, a.lng, geoLocation.value.latitude, geoLocation.value.longitude) - distance(b.lat, b.lng, geoLocation.value.latitude, geoLocation.value.longitude);
+            });
+        }
+    }
+
     return sorted;
 });
+
+
+const addRemoveFilter = (filter: PossibleFilters) => {
+    if (sortBy.value.includes(filter)) {
+        sortBy.value = sortBy.value.filter((f) => f !== filter);
+    } else {
+        sortBy.value.push(filter);
+    }
+}
 
 const tryGetGeoLocation = () => {
     if (navigator.geolocation) {
